9f8a4fa9af8d356a0f3e93bd4e33265028270039
Steven <ealsatie1866@yandex.com>
2018-07-23 20:49:28 +0100

    Add files via upload
    files for CV

---
+++ script.js
@@ -0,0 +1,13 @@
+
+/*function lightUp(){
+	document.getElementById('highlight1').style.backgroundColor 
+	= "#80ffff";
+}
+
+/*setTimeout(function(){
+	document.getElementById('highlight1').style.backgroundColor
+	.display = 'none';
+	}, 3000);*/
+
+
+
